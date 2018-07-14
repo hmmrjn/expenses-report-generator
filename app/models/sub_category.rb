29e8b5e83a3f3dabffe2cf1c96fa76fc07d13c68
hamajun <hmmrjn@gmail.com>
2018-07-15 00:59:53 +0900

    [add] sub_categories amount sums added a table with the sum of amount for each Sub Category in All Expenses page

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,4 +1,12 @@
 class SubCategory < ApplicationRecord
   belongs_to :category
+  has_many :expenses
   validates :name, :category, presence: true
+
+  def date_span
+    d1 = self.expenses.order(:date).first.date
+    d2 = self.expenses.order(:date).last.date
+    d2_format = (d1.month == d2.month)? ((d1.day == d2.day)? '/%Y' : '-%d/%Y') : '-%m/%d/%Y'
+    d1.strftime('%m/%d') + d2.strftime(d2_format)
+  end
 end
