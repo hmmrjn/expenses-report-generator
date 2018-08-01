313c53917299feabecfcadc90c201ad356b873ed
hamajun <hmmrjn@gmail.com>
2018-08-02 01:01:04 +0900

    add categories doughnut chart

--- app/controllers/analitics_controller.rb
+++ app/controllers/analitics_controller.rb
@@ -1,10 +1,21 @@
 class AnaliticsController < ApplicationController
   def index
-    sub_categories = SubCategory.all
-    @array = []
-    sub_categories.each do |sub_category|
-      @array.push({name: sub_category.name, amount: sub_category.expenses.sum(:amount)})
+    # categories doughnut chart
+    @categories_array = []
+    categories = Category.all
+    categories.each do |category|
+      sub_categories = SubCategory.where(category: category)
+      expenses = Expense.where(sub_category: sub_categories)
+      @categories_array.push({name: category.name, amount: expenses.sum(:amount)})
     end
-    @array.sort_by! {|row| row[:amount]}.reverse!
+    @categories_array.sort_by! {|row| row[:amount]}.reverse!
+
+    # sub_categories doughnut chart
+    @sub_categories_array = []
+    sub_categories = SubCategory.all
+    sub_categories.each do |sub_category|
+      @sub_categories_array.push({name: sub_category.name, amount: sub_category.expenses.sum(:amount)})
+    end
+    @sub_categories_array.sort_by! {|row| row[:amount]}.reverse!
   end
 end
