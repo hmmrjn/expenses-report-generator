06ad035011ec3468dd610abb39c93c8e14a38a80
hamajun <hmmrjn@gmail.com>
2018-08-02 17:41:56 +0900

    update order of sub_categories periodical column chart sorted by the sub_categories sum amount

--- app/controllers/analitics_controller.rb
+++ app/controllers/analitics_controller.rb
@@ -23,21 +23,22 @@ class AnaliticsController < ApplicationController
   end
 
   private
-    # retruns => {["03", "business meal (1)"]=>4352, ["03", "business meal (2)"]=>3240, ...}
+    # retruns => [{:month=>"05", :amounts=>[25000, 0,.., 800]}, {:month=>"06", :amounts=>[147730, 48760,.., 2210], ...}
     def prepare_montly_amounts_array
-      @sub_categories = SubCategory.all.order(:name)
+      @sub_category_names = Expense.joins(:sub_category).group('sub_categories.name').order('sum_amount DESC').sum(:amount).keys
+      # => {"pasmo charge"=>277870, "shinkansen"=>171860, "japanese class"=>138100, "
       # TODO strftime() works only on SQLite
       query_result = Expense.all.joins(:sub_category).group('strftime("%m", date)').group('sub_categories.name').sum('amount')
       month_last = query_result.first[0][0] # query_result.first => [["03", "business meal (1)"], 4352]
       result_array = []
-      temp_array = Array.new(@sub_categories.length).fill(0)
+      temp_array = Array.new(@sub_category_names.length).fill(0)
       query_result.each do |month_subcat_array, amount|
         if month_last != month_subcat_array[0]
           result_array << {month: month_last, amounts: temp_array}
-          temp_array = Array.new(@sub_categories.length).fill(0)
+          temp_array = Array.new(@sub_category_names.length).fill(0)
         end
-        @sub_categories.each_with_index do |sub_category, i|
-          temp_array[i] = amount if sub_category.name == month_subcat_array[1]
+        @sub_category_names.each_with_index do |sub_category_name, i|
+          temp_array[i] = amount if sub_category_name == month_subcat_array[1]
         end
         month_last = month_subcat_array[0]
       end
